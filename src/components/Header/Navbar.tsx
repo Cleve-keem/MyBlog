233105c19dbb3476128641dbc22daf4207928c0d
Cleve-keem <belloharyourmidey@gmail.com>
2025-08-25 10:12:19 -0500

    solved sm left error

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -9,12 +9,12 @@ export default function Navbar({ open }: NavbarProps) {
     <nav
       className={`absolute top-0 ${
         open ? "left-0" : "-left-100"
-      } bottom-0 w-[70%] px-5 py-4 bg-white transition-all duration-500 shadow-lg`}
+      } bottom-0 w-[70%] px-5 py-4 bg-white transition-all duration-500 shadow-lg sm:relative sm:w-fit sm:p-0 sm:bg-transparent sm:shadow-none sm:-left-0`}
     >
       <div className="sm:hidden py-7">
         <AuthButton />
       </div>
-      <ul className="mt-5 flex flex-col gap-4 font-semibold sm:hidden sm:gap-3">
+      <ul className="mt-5 flex flex-col gap-4 font-semibold sm:flex-row sm:gap-3 sm:mt-0">
         <li className="">
           <a href="#" className="">
             Home
