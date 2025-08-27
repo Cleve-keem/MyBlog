49ada1509c2782a5f9ad39cf9028f3720f2a152c
Cleve-keem <belloharyourmidey@gmail.com>
2025-08-27 03:35:07 -0500

    adjusted searchbar

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,8 +1,8 @@
 export default function SearchBar() {
   return (
-    <div className="w-1/2 flex">
-      <input type="text" className="w-full py-2 px-3 text-[18px] border border-gray-400 rounded outline-none" />
-      <span className="px-4 py-2 border">S</span>
+    <div className="w-1/2 flex border border-grey-400 rounded">
+      <input type="text" className="w-full py-2 px-3 text-[18px] outline-none" />
+      <span className="px-4 py-2 border-l">S</span>
     </div>
   );
 }
