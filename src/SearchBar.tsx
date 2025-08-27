0bf06a02da58df9dcef888513daa86022c328194
Cleve-keem <belloharyourmidey@gmail.com>
2025-08-27 03:55:22 -0500

    commented customSwiper component

--- src/SearchBar.tsx
+++ src/SearchBar.tsx
@@ -1,7 +1,10 @@
 export default function SearchBar() {
   return (
     <div className="w-1/2 flex border border-grey-400 rounded">
-      <input type="text" className="w-full py-2 px-3 text-[18px] outline-none" />
+      <input
+        type="text"
+        className="w-full py-2 px-3 text-[18px] outline-none"
+      />
       <span className="px-4 py-2 border-l">S</span>
     </div>
   );
--- src/components/CustomSwiper.tsx
+++ src/components/CustomSwiper.tsx
@@ -1,20 +1,20 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-export default function CustomSwiper() {
-  const [currentIndex, setCurrentIndex] = useState<number>(0);
-  const slides: string[] = [
-    "slide1.jpg",
-    "slide-2.jpg",
-    "slide-3.jpg",
-    "slide-4.jpg",
-    "slide-5.jpg",
-  ];
-  return (
-    <div className="w-full">
-      {slides.map((src, i) => (
-        <img src={src} key={i} />
-      ))}
-      {/* <img src="/slide1.jpg" alt="" /> */}
-    </div>
-  );
-}
+// export default function CustomSwiper() {
+//   const [currentIndex, setCurrentIndex] = useState<number>(0);
+//   const slides: string[] = [
+//     "slide1.jpg",
+//     "slide-2.jpg",
+//     "slide-3.jpg",
+//     "slide-4.jpg",
+//     "slide-5.jpg",
+//   ];
+//   return (
+//     <div className="w-full">
+//       {slides.map((src, i) => (
+//         <img src={src} key={i} />
+//       ))}
+//       {/* <img src="/slide1.jpg" alt="" /> */}
+//     </div>
+//   );
+// }
