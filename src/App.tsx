28d49481387a343e58594d8971d46a5e9a0004b5
Cleve-keem <belloharyourmidey@gmail.com>
2025-09-02 16:32:02 -0500

    created auth components and layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import AppLayout from "./layouts/AppLayout";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Blogs from "./pages/Blogs";
 import Contact from "./pages/Contact";
+import AuthLayout from "./layouts/AuthLayout";
+import Login from "./pages/auths/Login";
+import Signin from "./pages/auths/Signin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route element={<AuthLayout />}>
+          <Route index element={<Navigate replace to="/auth-login" />} />
+          <Route path="/auth-login" element={<Login />} />
+          <Route path="/auth-sign-up" element={<Signin />} />
+        </Route>
+
         <Route element={<AppLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
