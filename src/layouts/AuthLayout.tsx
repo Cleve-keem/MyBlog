4ddb96ef6b727c80f4c688671632fde9da98b9bc
Cleve-keem <108543475+Cleve-keem@users.noreply.github.com>
2025-09-04 03:09:17 +0100

    Update AuthLayout.tsx
    Reduced the width

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div className="h-screen flex flex-col justify-center items-center md:max-w-1/2 mx-auto">
+    <div className="h-screen flex flex-col justify-center items-center w-[90%] md:max-w-1/2 mx-auto">
       <Outlet />
     </div>
   );
