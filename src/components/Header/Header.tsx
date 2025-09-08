2f6047babfba0d85d547bb84c931a0af51a8c9d7
Cleve-keem <belloharyourmidey@gmail.com>
2025-09-08 18:47:15 -0500

    increased header height

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -13,7 +13,7 @@ export default function Header() {
   const toggleMobile = () => setIsOpen((prev) => !prev);
 
   return (
-    <motion.header className="h-[50px] fixed left-0 right-0 top-1.5 px-6 flex justify-between sm:px-2 lg:w-[90%] mx-auto rounded hover:bg-white/60 hover:backdrop-blur-md hover:shadow-md hover:scale-[1.01] z-40">
+    <motion.header className="h-[65px] fixed left-0 right-0 top-0 md:top-1.5 px-6 flex justify-between sm:px-2 lg:w-[90%] mx-auto rounded hover:bg-white/60 hover:backdrop-blur-md hover:shadow-md hover:scale-[1.01] z-40 bg-white">
       {/* Left */}
       <div className="flex items-center gap-5">
         <Hamburger closeNav={toggleMobile} />
