45c14bc40556046042d46a68157700e2a9f7bf74
Cleve-keem <belloharyourmidey@gmail.com>
2025-09-28 18:05:16 -0500

    removed min-h-screen from auth Layout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router";
 
 export default function AuthLayout() {
   return (
-    <div className="w-[90%] min-h-screen flex flex-col items-center py-10 md:max-w-1/2 mx-auto">
+    <div className="w-[90%] flex flex-col items-center py-10 md:max-w-1/2 mx-auto">
       <Outlet />
     </div>
   );
